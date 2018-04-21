87ee1decef39331f4760644056c16c62b5bc7f06
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-04-21 13:33:59 +0200

    Add last sections, map, with box contact, add also api key for google maps

--- js/script.js
+++ js/script.js
@@ -1,10 +1,181 @@
-//Map
 
-function myMap() {
- var mapOptions = {
-     center: new google.maps.LatLng(51.5, -0.12),
-     zoom: 10,
-     mapTypeId: google.maps.MapTypeId.HYBRID
- }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+function initMap() {
+ var uluru = {lat: -37.815302, lng: 144.865231};
+ var map = new google.maps.Map(document.getElementById('map'), {
+   zoom: 13,
+   center: uluru
+ });
+ // Styles a map in night mode.
+ var map = new google.maps.Map(document.getElementById('map'), {
+  center: uluru,
+  zoom: 12,
+  styles: [
+   {
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#f5f5f5"
+       }
+     ]
+   },
+   {
+     "elementType": "labels.icon",
+     "stylers": [
+       {
+         "visibility": "off"
+       }
+     ]
+   },
+   {
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#616161"
+       }
+     ]
+   },
+   {
+     "elementType": "labels.text.stroke",
+     "stylers": [
+       {
+         "color": "#f5f5f5"
+       }
+     ]
+   },
+   {
+     "featureType": "administrative.land_parcel",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#bdbdbd"
+       }
+     ]
+   },
+   {
+     "featureType": "poi",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#eeeeee"
+       }
+     ]
+   },
+   {
+     "featureType": "poi",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#757575"
+       }
+     ]
+   },
+   {
+     "featureType": "poi.park",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#e5e5e5"
+       }
+     ]
+   },
+   {
+     "featureType": "poi.park",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#9e9e9e"
+       }
+     ]
+   },
+   {
+     "featureType": "road",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#ffffff"
+       }
+     ]
+   },
+   {
+     "featureType": "road.arterial",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#757575"
+       }
+     ]
+   },
+   {
+     "featureType": "road.highway",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#dadada"
+       }
+     ]
+   },
+   {
+     "featureType": "road.highway",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#616161"
+       }
+     ]
+   },
+   {
+     "featureType": "road.local",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#9e9e9e"
+       }
+     ]
+   },
+   {
+     "featureType": "transit.line",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#e5e5e5"
+       }
+     ]
+   },
+   {
+     "featureType": "transit.station",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#eeeeee"
+       }
+     ]
+   },
+   {
+     "featureType": "water",
+     "elementType": "geometry",
+     "stylers": [
+       {
+         "color": "#c9c9c9"
+       }
+     ]
+   },
+   {
+     "featureType": "water",
+     "elementType": "labels.text.fill",
+     "stylers": [
+       {
+         "color": "#9e9e9e"
+       }
+     ]
+   }
+ ]
+});
+var marker = new google.maps.Marker({
+ position: {
+  lat: -37.815030,
+  lng: 144.960331
+ },
+ map: map
+});
 }
+
